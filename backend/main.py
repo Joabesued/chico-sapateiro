56bec7af763f5ed69e84a9a5962e629d04736c73
Joabe <costa.joabe17@gmail.com>
2026-05-18 23:11:39 -0300

    fix: abre CORS para todos os origins

--- backend/main.py
+++ backend/main.py
@@ -22,12 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "https://chico-sapateiro.vercel.app",
-        "https://chico-sapateiro-2wt5rj325-joabesueds-projects.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
